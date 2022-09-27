ac4e7fad72bf069c10f911dbd623c3c9a615ac49
Project-WWW <91307684+Project-WWW@users.noreply.github.com>
2022-09-27 14:58:22 +0300

    Add files via upload

---
+++ sli_der.js
@@ -0,0 +1,11 @@
+$(".rev_slider").slick({
+  infinite: true,
+  arrows: false,
+  dots: false,
+  speed: 2000,
+  easign: "ease",
+  infinite: true,
+  autoplay: true,
+  fade: true,
+  pauseOnHover: false,
+});
